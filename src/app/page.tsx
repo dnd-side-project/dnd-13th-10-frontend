87bb2832e82231cf1446814fa28ec3c3abf921b9
bbjbc <jobc4550@gmail.com>
2025-07-23 23:12:36 +0900

    chore: husky를 통한 git hook 설정 및 commitlint 추가 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,35 @@
-"use client";
+'use client';
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Page() {
   const { data: session, status } = useSession();
 
-  if (status === "loading") return <p>Loading...</p>;
+  if (status === 'loading') return <p>Loading...</p>;
 
   return (
     <div>
-      {status === "authenticated" ? (
+      {status === 'authenticated' ? (
         <>
           <p>Welcome, {session?.user?.name}</p>
-          <button onClick={() => signOut()}>Sign Out</button>
+          <button
+            className="flex gap-5 rounded-lg bg-amber-400 p-5 text-blue-500"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
         </>
       ) : (
         <>
           <p>Not signed in</p>
           <br />
-          <button onClick={() => signIn("kakao")}>Sign In with Kakao</button>
+          <button onClick={() => signIn('github')}>Sign In with GitHub</button>
           <br />
-          <button onClick={() => signIn("naver")}>Sign In with Naver</button>
+          <button onClick={() => signIn('kakao')}>Sign In with Kakao</button>
           <br />
-          <button onClick={() => signIn("google")}>Sing In With Google</button>
+          <button onClick={() => signIn('naver')}>Sign In with Naver</button>
+          <br />
+          <button onClick={() => signIn('google')}>Sing In With Google</button>
         </>
       )}
     </div>
