47b64fffe4f2ad9c9f43a0686cd327347b1b1b4d
bbjbc <jobc4550@gmail.com>
2025-08-08 00:00:41 +0900

    feat: 네비게이션 바에서 사용되는 경로 상수 정의 (#7)

---
+++ src/constants/path.ts
@@ -0,0 +1,6 @@
+export const PATH = {
+  HOME: { PATH: '/home', LABEL: '홈' },
+  CALENDAR: { PATH: '/calendar', LABEL: '일정' },
+  COMMUNITY: { PATH: '/community', LABEL: '커뮤니티' },
+  MY: { PATH: '/my-page', LABEL: 'MY' },
+};
