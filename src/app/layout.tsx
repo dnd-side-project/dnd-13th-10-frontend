6345f29c11edb3e4f98bfbd7816b5129568a19ab
nno3onn <nno3onn@naver.com>
2025-08-15 16:22:23 +0900

    style: 레이아웃 중앙 정렬  (#19)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body className={`${pretendard.variable} font-pretendard antialiased`}>
         <NextAuthProvider>
           <QueryClientProvider>
-            <div className="bg-foundation-bg max-w-maxWidth mx-auto flex min-h-screen w-full flex-col text-white">
+            <div className="bg-foundation-bg max-w-maxWidth Justify-center mx-auto flex h-screen min-h-screen flex-col items-center text-white">
               {children}
             </div>
             <ReactQueryDevtools />
