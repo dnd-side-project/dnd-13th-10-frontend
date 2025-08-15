a33d3d2ece8dcc6f4c52ab6065da8c51cf2b67eb
bbjbc <jobc4550@gmail.com>
2025-08-16 00:15:11 +0900

    refactor: simplify type definitions using ValueOf utility type (#25)

--- src/types/memoirTypes.ts
+++ src/types/memoirTypes.ts
@@ -11,23 +11,18 @@ import {
   SATISFACTION_NOTE,
 } from '@/constants/code';
 
-export type MemoirType = (typeof MEMOIR_TYPES)[keyof typeof MEMOIR_TYPES];
-export type InterviewFormat =
-  (typeof INTERVIEW_FORMAT)[keyof typeof INTERVIEW_FORMAT];
-export type InterviewLevel =
-  (typeof INTERVIEW_LEVEL)[keyof typeof INTERVIEW_LEVEL];
-export type InterviewMethod =
-  (typeof INTERVIEW_METHOD)[keyof typeof INTERVIEW_METHOD];
-export type InterviewMood =
-  (typeof INTERVIEW_MOOD)[keyof typeof INTERVIEW_MOOD];
-export type InterviewStatus =
-  (typeof INTERVIEW_STATUS)[keyof typeof INTERVIEW_STATUS];
-export type InterviewStep =
-  (typeof INTERVIEW_STEP)[keyof typeof INTERVIEW_STEP];
-export type Position = (typeof POSITION)[keyof typeof POSITION];
-export type QuestionType = (typeof QUESTION_TYPE)[keyof typeof QUESTION_TYPE];
-export type SatisfactionNote =
-  (typeof SATISFACTION_NOTE)[keyof typeof SATISFACTION_NOTE];
+type ValueOf<T> = T[keyof T];
+
+export type MemoirType = ValueOf<typeof MEMOIR_TYPES>;
+export type InterviewFormat = ValueOf<typeof INTERVIEW_FORMAT>;
+export type InterviewLevel = ValueOf<typeof INTERVIEW_LEVEL>;
+export type InterviewMethod = ValueOf<typeof INTERVIEW_METHOD>;
+export type InterviewMood = ValueOf<typeof INTERVIEW_MOOD>;
+export type InterviewStatus = ValueOf<typeof INTERVIEW_STATUS>;
+export type InterviewStep = ValueOf<typeof INTERVIEW_STEP>;
+export type Position = ValueOf<typeof POSITION>;
+export type QuestionType = ValueOf<typeof QUESTION_TYPE>;
+export type SatisfactionNote = ValueOf<typeof SATISFACTION_NOTE>;
 
 export interface QuestionRequest {
   question: string;
