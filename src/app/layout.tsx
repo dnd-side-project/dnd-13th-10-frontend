3e4cb5c7038ecb0fcdac8207a8c5b1249d558f06
nno3onn <nno3onn@naver.com>
2025-08-16 16:03:09 +0900

    fix: 오타 수정  (#19)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body className={`${pretendard.variable} font-pretendard antialiased`}>
         <NextAuthProvider>
           <QueryClientProvider>
-            <div className="bg-foundation-bg max-w-maxWidth Justify-center mx-auto flex h-screen min-h-screen flex-col items-center text-white">
+            <div className="bg-foundation-bg max-w-maxWidth mx-auto flex h-screen min-h-screen flex-col items-center justify-center text-white">
               {children}
             </div>
             <ReactQueryDevtools />
