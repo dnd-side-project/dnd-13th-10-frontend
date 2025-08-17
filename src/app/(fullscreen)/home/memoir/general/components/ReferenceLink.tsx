d30bb98e14f47e4e305eb52365376dabaa596f13
bbjbc <jobc4550@gmail.com>
2025-08-17 22:59:12 +0900

    feat: 첨부 또는 링크 섹션 컴포넌트 제작 (#35)

---
+++ src/app/(fullscreen)/home/memoir/general/components/ReferenceLink.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Input } from '@/components/ui/Input';
+import { Label } from '@/components/ui/Label';
+
+import Title from './Title';
+import { useGeneralMemoirFormStore } from '../store/generalMemoirFormStore';
+
+export default function ReferenceLink() {
+  const data = useGeneralMemoirFormStore(state => state.formData.references);
+  const updateFormData = useGeneralMemoirFormStore(
+    state => state.updateFormData,
+  );
+
+  const handleFieldChange = (field: keyof typeof data, value: string) => {
+    updateFormData('references', { ...data, [field]: value });
+  };
+
+  return (
+    <section>
+      <Title title="첨부 또는 링크" />
+      <div className="flex flex-col gap-2">
+        <Label label="URL" htmlFor="url" />
+        <Input
+          id="url"
+          name="url"
+          value={data.url}
+          placeholder="http://"
+          onChange={e => handleFieldChange('url', e.target.value)}
+        />
+      </div>
+    </section>
+  );
+}
