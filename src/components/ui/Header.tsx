bb8007d7dfa396ecd9ab109f96c4dbf03dd0d8d7
bbjbc <jobc4550@gmail.com>
2025-08-23 18:49:08 +0900

    style: header 컴포넌트 스타일 수정 (#51)

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -6,7 +6,7 @@ import LeftArrowIcon from '@/assets/icon/left_arrow_icon.svg';
 import { cn } from '@/utils/cn';
 
 interface Props {
-  title: string;
+  title: ReactNode;
   rightContent?: ReactNode | string;
   onBackClick?: () => void;
   onRightClick?: () => void;
@@ -29,35 +29,49 @@ function Header({
   };
 
   return (
-    <header className="border-foundation-divider relative border py-6">
+    <header
+      className={cn(
+        'border-foundation-divider flex items-center border px-4',
+        typeof title === 'string' ? 'py-6' : 'py-3',
+      )}
+    >
       {showBackButton && (
-        <LeftArrowIcon
-          className="absolute top-1/2 left-6 -translate-y-1/2 cursor-pointer"
-          aria-label="뒤로 가기"
-          onClick={handleBackClick}
-        />
+        <div className="w-8 shrink-0">
+          <LeftArrowIcon
+            className="shrink-0 cursor-pointer"
+            aria-label="뒤로 가기"
+            onClick={handleBackClick}
+          />
+        </div>
       )}
 
-      <div className={cn('text-center', !showBackButton && 'px-6')}>
-        <span className="typo-headline text-foundation-strong">{title}</span>
+      <div className="flex-1 text-center">
+        {typeof title === 'string' ? (
+          <span className="typo-headline text-foundation-strong">{title}</span>
+        ) : (
+          title
+        )}
       </div>
 
-      <div
-        className={cn(
-          'absolute top-1/2 right-6 flex -translate-y-1/2 items-center',
-          onRightClick && 'cursor-pointer',
-        )}
-        onClick={onRightClick}
-      >
-        {rightContent &&
-          (typeof rightContent === 'string' ? (
+      {rightContent ? (
+        <div
+          className={cn(
+            'flex w-8 flex-shrink-0 justify-end',
+            onRightClick && 'cursor-pointer',
+          )}
+          onClick={onRightClick}
+        >
+          {typeof rightContent === 'string' ? (
             <span className="typo-body-02 text-foundation-strong">
               {rightContent}
             </span>
           ) : (
             rightContent
-          ))}
-      </div>
+          )}
+        </div>
+      ) : showBackButton ? (
+        <div className="w-8 shrink-0" />
+      ) : null}
     </header>
   );
 }
