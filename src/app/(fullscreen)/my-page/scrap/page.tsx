a61a30f251d986fa6d4365377011652c556a973f
Boongranii <jobc4550@gmail.com>
2025-08-24 18:15:52 +0900

    [✨ design] overflow시 배경 잘리는 스타일 수정 (#59)

--- src/app/(fullscreen)/my-page/scrap/page.tsx
+++ src/app/(fullscreen)/my-page/scrap/page.tsx
@@ -5,9 +5,9 @@ import { mockMemoirs } from '../mocks/memoir';
 
 export default function MyScrapPage() {
   return (
-    <main className="flex h-screen flex-col">
+    <main className="flex min-h-screen flex-col">
       <Header title="내가 스크랩한 글" />
-      <div className="mt-8">
+      <div className="my-8">
         <MemoirList memoirs={mockMemoirs} emptyText="스크랩한 글이 없어요." />
       </div>
     </main>
