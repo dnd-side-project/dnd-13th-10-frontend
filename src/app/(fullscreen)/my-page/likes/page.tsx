836f5caca46f6afa5edb01905a53c82f9170b6f9
bbjbc <jobc4550@gmail.com>
2025-08-24 15:26:29 +0900

    style: 마이페이지 풀스크린 스타일 수정 (#57)

--- src/app/(fullscreen)/my-page/likes/page.tsx
+++ src/app/(fullscreen)/my-page/likes/page.tsx
@@ -7,7 +7,9 @@ export default function MyLikesPage() {
   return (
     <main className="flex h-screen flex-col">
       <Header title="내가 좋아요한 글" />
-      <MemoirList memoirs={mockMemoirs} emptyText="좋아요한 글이 없어요." />
+      <div className="mt-8">
+        <MemoirList memoirs={mockMemoirs} emptyText="좋아요한 글이 없어요." />
+      </div>
     </main>
   );
 }
