808766e319e6fe22b9c50077f4387e11156ef9dd
nno3onn <nno3onn@naver.com>
2025-08-25 22:33:19 +0900

    fix: 홈 리다이렉트 에러 해결 (#63)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@
 import OnboardingView from '@/components/onboarding/OnboardingView';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
   const router = useRouter();
   const { status } = useSession();
 
-  if (status === 'authenticated') {
-    router.push('/home');
-  }
+  useEffect(() => {
+    if (status === 'authenticated') router.replace('/home');
+  }, [status, router]);
 
   return <OnboardingView />;
 }
