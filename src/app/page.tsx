7c5de8af79546a237b4840bac9f5c850e4db67ef
nno3onn <nno3onn@naver.com>
2025-08-25 22:05:14 +0900

    feat: 로그인 시 홈으로 리다이렉트  (#63)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+'use client';
+
 import OnboardingView from '@/components/onboarding/OnboardingView';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
+  const { status } = useSession();
+
+  if (status === 'authenticated') {
+    router.push('/home');
+  }
+
   return <OnboardingView />;
 }
