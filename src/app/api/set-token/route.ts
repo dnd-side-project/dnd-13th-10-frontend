9f4ce02659d4a001d30c5f208b9dee8d412cf583
nno3onn <nno3onn@naver.com>
2025-08-26 23:23:05 +0900

    feat: 토큰 쿠키 설정/해지 라우트  (#72)

--- src/app/api/set-token/route.ts
+++ src/app/api/set-token/route.ts
@@ -1,10 +1,10 @@
 export async function POST(req: Request) {
-  const { token } = await req.json();
+  const { token, maxAgeSec = 60 * 60 } = await req.json(); // 기본 1시간
 
   const res = new Response(null, { status: 204 });
   res.headers.append(
     'Set-Cookie',
-    `${encodeURIComponent('devseed_token')}=${encodeURIComponent(token)}; Path=/; HttpOnly; SameSite=Lax${process.env.NODE_ENV === 'production' ? '; Secure' : ''}`,
+    `${encodeURIComponent('devseed_token')}=${encodeURIComponent(token)}; Path=/; Max-Age=${maxAgeSec}; HttpOnly; SameSite=Lax${process.env.NODE_ENV === 'production' ? '; Secure' : ''}`,
   );
   return res;
 }
