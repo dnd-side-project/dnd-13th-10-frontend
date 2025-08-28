57f88952fe74c0fed2d0c9b8b1bf0c513040e0fb
nno3onn <nno3onn@naver.com>
2025-08-29 02:33:37 +0900

    feat: 내가 댓글을 작성한 회고 글 조회 기능 구현  (#71)

--- src/app/(fullscreen)/my-page/comments/page.tsx
+++ src/app/(fullscreen)/my-page/comments/page.tsx
@@ -1,14 +1,39 @@
+'use client';
+
 import { Header } from '@/components/ui/Header';
 
 import MemoirList from '../components/MemoirList';
-import { mockMemoirs } from '../mocks/memoir';
+import { memoirInfiniteQueries } from '@/queries/memoirOptions';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
 
 export default function MyCommentsPage() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery(memoirInfiniteQueries.commented());
+
+  const memoirs = data?.pages.flatMap(p => p.data.result) ?? [];
+
+  const loaderRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (!loaderRef.current) return;
+    const io = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      },
+      { rootMargin: '200px' },
+    );
+    io.observe(loaderRef.current);
+    return () => io.disconnect();
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
   return (
     <main className="flex min-h-screen flex-col">
       <Header title="내가 댓글 남긴 글" />
       <div className="my-8">
-        <MemoirList memoirs={mockMemoirs} emptyText="작성한 댓글이 없어요." />
+        <MemoirList memoirs={memoirs} emptyText="작성한 댓글이 없어요." />
+        <div ref={loaderRef} />
       </div>
     </main>
   );
