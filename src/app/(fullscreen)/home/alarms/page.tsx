a111b8c057e74cc70cde5c4738e16a67751e5614
bbjbc <jobc4550@gmail.com>
2025-08-29 22:48:06 +0900

    feat: 알람 조회 api 연동 (#93)

--- src/app/(fullscreen)/home/alarms/page.tsx
+++ src/app/(fullscreen)/home/alarms/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 
 import { Header } from '@/components/ui/Header';
 
+import AlarmList from './components/AlarmList';
+
 export const metadata: Metadata = {
   title: '알림',
   description: '면접 일정에 대한 알림을 확인하고 관리해보세요.',
@@ -9,8 +11,9 @@ export const metadata: Metadata = {
 
 export default function AlarmsPage() {
   return (
-    <>
-      <Header title="알림" />
-    </>
+    <main className="flex min-h-dvh flex-col">
+      <Header title="알람" />
+      <AlarmList />
+    </main>
   );
 }
