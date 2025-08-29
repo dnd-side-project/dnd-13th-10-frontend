cf5174b98bfb8baf7075846babf3812899ef8948
Boongranii <jobc4550@gmail.com>
2025-08-30 00:19:37 +0900

    [✨ feat] 회고 임시 저장 처리한 회고 다시 불러와 수정할 수 있도록 제작 (#99) (#100)

--- src/app/(fullscreen)/my-page/components/MemoirList.tsx
+++ src/app/(fullscreen)/my-page/components/MemoirList.tsx
@@ -7,15 +7,26 @@ interface Props {
   memoirs: ApiMemoirItem[];
   hideBadge?: boolean;
   emptyText: string;
+  itemHrefPattern?: string;
 }
 
-export default function MemoirList({ memoirs, hideBadge, emptyText }: Props) {
+export default function MemoirList({
+  memoirs,
+  hideBadge,
+  emptyText,
+  itemHrefPattern,
+}: Props) {
   return (
     <>
       <div className="mb-5 flex flex-1 flex-col gap-3 px-5">
         {memoirs.length > 0 ? (
           memoirs.map(memoir => (
-            <MemoirItem key={memoir.id} memoir={memoir} hideBadge={hideBadge} />
+            <MemoirItem
+              key={memoir.id}
+              memoir={memoir}
+              hideBadge={hideBadge}
+              hrefPattern={itemHrefPattern}
+            />
           ))
         ) : (
           <EmptyState text={emptyText} />
